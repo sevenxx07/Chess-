f8c6d17edc65b7343b3ce4c6f19393e01195822a
Sára Veselá <104825252+sevenxx07@users.noreply.github.com>
2024-08-01 14:03:25 +0200

    Create Main.java

---
+++ src/main/java/chessclient/Main.java
@@ -0,0 +1,12 @@
+package cz.cvut.fel.pjv.chessclient;
+
+
+import cz.cvut.fel.pjv.chessclient.gui.ChessApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ChessApp.main(args);
+    }
+
+}
